bdf9f047f3bdc8a51fbd484622e8d97a98814a49
ashishkiran93 <ashishkiran93@gmail.com>
2021-07-12 08:56:08 -0700

    my first commit

---
+++ src/components/Topbar/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./topbar.css";
+import { connect } from 'react-redux';
+import { adminLogout } from '../../actions';
+import { useHistory } from "react-router-dom";
+
+
+
+const Topbar = ({ loginStatus, logOutAdmin }) => {
+    const history = useHistory()
+
+    return (<div className="topbar">
+        <div><h1>WELCOME TO THE PHARMACY SHOP</h1></div>
+        <div className="topbar-menu">
+            {loginStatus && <p onClick={() => {
+                localStorage.setItem('loginStatus', false)
+                localStorage.setItem('adminLogin', false)
+                localStorage.setItem('salesLogin', false)
+                history.push("/");
+                logOutAdmin()
+            }} style={{ cursor: 'pointer' }}>Logout</p>}
+        </div>
+    </div>);
+}
+
+
+
+const mapStateToProps = (state) => ({
+    loginStatus: state.loginStatus
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    logOutAdmin: () => dispatch(adminLogout(''))
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Topbar)
